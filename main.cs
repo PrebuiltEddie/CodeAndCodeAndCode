93131c424f37f7e46900643826015f7c0bb67483
PrebuiltEddie <eddie121803@gmail.com>
2024-05-27 01:54:40 +0000

    PersonalInfo 1.1

--- main.cs
+++ main.cs
@@ -1,7 +1,25 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Test Case 1
+        DisplayPersonalInfo("Lucinda Potter", "6/24/1992", "000-000-0101", "000-000-0189");
+        // Test Case 2
+        DisplayPersonalInfo("John Smith", "8/15/1978", "000-111-0101", "000-111-0189");
+        // Test Case 3
+        DisplayPersonalInfo("Chris Johnson", "12/01/1987", "000-222-0101", "000-222-0189");
+        // Test Case 4
+        DisplayPersonalInfo("Robert Hall", "2/27/1949", "000-444-0101", "000-444-0189");
+    }
+
+    public static void DisplayPersonalInfo(string name, string birthdate, string workPhone, string cellPhone)
+    {
+        Console.WriteLine(name);
+        Console.WriteLine(birthdate);
+        Console.WriteLine("work " + workPhone);
+        Console.WriteLine("cell " + cellPhone);
+        Console.WriteLine();
+    }
 }
